8a4edbe34412315e171e7a272f628eaceedfced0
ParthGanjoo <parth21342@iiitd.ac.in>
2024-01-23 22:34:38 +0530

    updated link

--- src/Components/LoginCard.jsx
+++ src/Components/LoginCard.jsx
@@ -16,7 +16,7 @@ function Form() {
       // Check for specific email and password
       if (email === 'khushi@dumdum.com' && password === 'dumdum') {
         // Redirect to Google.com
-        window.location.href = 'https://www.google.com';
+        window.location.href = 'https://pinterest-main.vercel.app';
       } else {
         alert('Invalid email or password');
       }
